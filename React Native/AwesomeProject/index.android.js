53867413c1671931136cbb46e3d76ec04d0e5876
Praise Tompane <tompanepp@gmail.com>
2016-12-26 20:18:26 +0200

    fix: learnt that registerComponent's second parameter can just be the root component I want as long as I keep the appkey 'AwesomeApp'

--- React Native/AwesomeProject/index.android.js
+++ React Native/AwesomeProject/index.android.js
@@ -4,7 +4,7 @@ import {AppRegistry, View} from 'react-native';
 class FlexDimensionsBasics extends Component {
     render() {
         return(
-            <View style={{flex: 1}}>
+            <View style={{flex: 1, flexDirection: 'column'}}>
                 <View style={{flex: 1, backgroundColor: 'powderblue'}}/>
                 <View style={{flex: 2, backgroundColor: 'skyblue'}}/>        
                 <View style={{flex: 3, backgroundColor: 'red'}}/>                                 
@@ -13,11 +13,4 @@ class FlexDimensionsBasics extends Component {
     }
 };
 
-//I want to import this, too much repetitions
-class AwesomeProject extends Component {
-    render(){
-     return(<FlexDimensionsBasics/>);
-    }
-};
-
-AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
+AppRegistry.registerComponent('df', () => FlexDimensionsBasics);
