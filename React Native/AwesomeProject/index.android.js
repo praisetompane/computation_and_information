090d89f1142015238da53b9840b9072de0c38eb6
Praise Tompane <tompanepp@gmail.com>
2016-12-26 20:05:43 +0200

    fix: moved some stuff around and worked through component dimensions

--- React Native/AwesomeProject/index.android.js
+++ React Native/AwesomeProject/index.android.js
@@ -1,41 +1,23 @@
-import React, {Component} from 'react';
-import {AppRegistry, StyleSheet, Text, View} from 'react-native';
+import React, { Component} from 'react';
+import {AppRegistry, View} from 'react-native';
 
-
-class ListOfStyles extends Component{
-    render(){
+class FlexDimensionsBasics extends Component {
+    render() {
         return(
-
-            <View>
-                <Text style={styles.red}> just red</Text>
-                <Text style={styles.bigblue}> just bigblue</Text>                
-                <Text style={{color: 'red'}}> another red</Text>                                             
-                <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
-                <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>            
+            <View style={{flex: 1}}>
+                <View style={{flex: 1, backgroundColor: 'powderblue'}}/>
+                <View style={{flex: 2, backgroundColor: 'skyblue'}}/>        
+                <View style={{flex: 3, backgroundColor: 'red'}}/>                                 
             </View>
         );
     }
-}
+};
 
-const styles = StyleSheet.create(
-    {
-        bigblue: {
-            color: 'blue',
-            fontWeight: 'bold',
-            fontSize: 30
-        },
-        red: {
-            color: 'red'
-        },
-    }
-);
-
+//I want to import this, too much repetition
 class AwesomeProject extends Component {
     render(){
-     return(<View> 
-                <ListOfStyles/>
-            </View>);
+     return(<FlexDimensionsBasics/>);
     }
-}
+};
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
