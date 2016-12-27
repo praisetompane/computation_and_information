e783371f019b9926c41955f5a241772f884a54db
Praise Tompane <tompanepp@gmail.com>
2016-12-27 10:16:53 +0200

    feauture: scroll view

--- React Native/AwesomeProject/index.android.js
+++ React Native/AwesomeProject/index.android.js
@@ -1,26 +1,33 @@
 import React, { Component} from 'react';
-import {AppRegistry, View, TextInput, Text} from 'react-native';
+import {AppRegistry, View, ScrollView, Image, Text} from 'react-native';
 
-
-class PizzaTranslator extends Component {
-    constructor(props){
-        super(props);
-        this.state = {text: ''}
-    }
-
-    render() {
+class IScroll extends Component {
+    render(){
         return(
-        <View style = {{padding: 10}}>
-            <TextInput style = {{height: 40}} 
-                       placeholder="Type here to translate!"
-                       onChangeText={(text) => this.setState({text})}/>
-        
-            <Text style={{padding: 10, fontSize: 42}}>
-                {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
-            </Text>
-        </View>
+            <ScrollView>
+                <Text style={{fontSize: 96}}>Scroll me plz</Text>
+                <Image source={require('./img/favicon.png')}/>
+                <Image source={require('./img/favicon.png')}/>
+                <Image source={require('./img/favicon.png')}/>
+                <Image source={require('./img/favicon.png')}/>
+                <Image source={require('./img/favicon.png')}/>
+                <Text style={{fontSize: 96}}>If you like that</Text>
+                <Image source={require('./img/favicon.png')}/>
+                <Image source={require('./img/favicon.png')}/>
+                <Image source={require('./img/favicon.png')}/>
+                <Image source={require('./img/favicon.png')}/>
+                <Image source={require('./img/favicon.png')}/>
+                <Image source={require('./img/favicon.png')}/>
+                <Text style={{fontSize: 96}}>Scrolling down</Text>
+                <Image source={require('./img/favicon.png')}/>
+                <Image source={require('./img/favicon.png')}/>
+                <Image source={require('./img/favicon.png')}/>
+                <Image source={require('./img/favicon.png')}/>
+                <Image source={require('./img/favicon.png')}/>
+                <Image source={require('./img/favicon.png')}/>                
+            </ScrollView>
         );
     }
 }
 
-AppRegistry.registerComponent('AwesomeProject', () => PizzaTranslator);
+AppRegistry.registerComponent('AwesomeProject', () => IScroll);
