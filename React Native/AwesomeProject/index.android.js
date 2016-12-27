a3fc3ecc23a30c3340450ab5112750515c99e5a4
Praise Tompane <tompanepp@gmail.com>
2016-12-27 09:54:10 +0200

    feature: user input

--- React Native/AwesomeProject/index.android.js
+++ React Native/AwesomeProject/index.android.js
@@ -1,20 +1,26 @@
 import React, { Component} from 'react';
-import {AppRegistry, View} from 'react-native';
+import {AppRegistry, View, TextInput, Text} from 'react-native';
 
-class AlignContentBasics extends Component {
+
+class PizzaTranslator extends Component {
+    constructor(props){
+        super(props);
+        this.state = {text: ''}
+    }
+
     render() {
         return(
-            <View style={{flex: 1, 
-                          flexDirection: 'column',
-                          justifyContent: 'space-between', //Available options = flex-start, center, flex-end, space-around, and space-between.
-                          alignItems: 'center'  
-                        }}>
-                <View style={{flex: 1, backgroundColor: 'powderblue'}}/>
-                <View style={{flex: 2, backgroundColor: 'skyblue'}}/>        
-                <View style={{flex: 3, backgroundColor: 'red'}}/>                                 
-            </View>
+        <View style = {{padding: 10}}>
+            <TextInput style = {{height: 40}} 
+                       placeholder="Type here to translate!"
+                       onChangeText={(text) => this.setState({text})}/>
+        
+            <Text style={{padding: 10, fontSize: 42}}>
+                {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+            </Text>
+        </View>
         );
     }
-};
+}
 
-AppRegistry.registerComponent('AwesomeProject', () => AlignContentBasics);
+AppRegistry.registerComponent('AwesomeProject', () => PizzaTranslator);
