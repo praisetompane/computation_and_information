ab24d3384078d9aa8775d3b555eec820b4ca2d37
Praise Tompane <tompanepp@gmail.com>
2016-12-28 05:00:16 +0200

    feature: better navigation

--- React Native/AwesomeProject/index.android.js
+++ React Native/AwesomeProject/index.android.js
@@ -1,36 +1,32 @@
-import React, { Component} from 'react';
-import {AppRegistry, View, ListView, Image, Text, Navigator} from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, Navigator } from 'react-native';
 
 import MyScene from './MyScene';
 
 class SimpleNavigationApp extends Component {
-    render(){
-        /*
-            route = object that contains info about a scene
-                provides context to the Navigator renderScene function to render a scene
-        */
-        return(<Navigator
-                    initialRoute={{title: 'My Initial Scence', index:0}}
-                    renderScene={(route, navigator) => {
-                    <MyScene 
+    render() {
+        return (
+            <Navigator
+                initialRoute={{ title: 'My Initial Scene', index: 0 }}
+                renderScene={(route, navigator) =>
+                    <MyScene
                         title={route.title}
                         onForward={() => {
-                                const nextIndex = route.index + 1;
-                                navigator.push({
-                                    title: 'Scence' + nextIndex,
-                                    index: nextIndex
-                                });
-                            }
-                        }
+                            const nextIndex = route.index + 1;
+                            navigator.push({
+                                title: 'Scene' + nextIndex,
+                                index: nextIndex,
+                            });
+                        }}
                         onBack={() => {
-                                if(route.index > 0){
-                                    navigator.pop();
-                                }
+                            if (route.index > 0) {
+                                navigator.pop();
                             }
                         }
-                      />
-                    }}/>
-                );
+                        }
+                        />
+                } />
+        );
     }
 }
 
