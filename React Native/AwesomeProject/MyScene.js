0ebed16ccf34be6ff4c3e3ad7ba2ed8a6db5b10f
Praise Tompane <tompanepp@gmail.com>
2016-12-28 04:05:16 +0200

    feature: scrolling, user input and navigation

--- React Native/AwesomeProject/MyScene.js
+++ React Native/AwesomeProject/MyScene.js
@@ -2,18 +2,17 @@ import React, {Component} from 'react';
 import { View, Text, Navigator} from 'react-native';
 
 export default class MyScene extends Component {
-    /*static getDefaultProps(){
-        return {
-            title: 'MyScene'
-        };
-    }*/
-
-    static defaultProps = {title: 'MyScene'}
-
     render(){
         return(
             <View>
-                <Text>Hi! My Name is {this.props.title}.</Text>
+                <Text>Current Scene: {this.props.title}.</Text>
+                <TouchableHighlight onPress={this.props.onFoward}>
+                    <Text>Tap me to load the next scene</Text>
+                </TouchableHighlight>
+
+                <TouchableHighlight onPress={this.props.onBack}>
+                    <Text>Tap me to go back</Text>
+                </TouchableHighlight>
             </View>
         );
     }
