78345af84e7023a5a7fdcbbbca596268797523f0
Praise Tompane <tompanepp@gmail.com>
2017-02-19 13:50:31 +0200

    feature: update Money implementation(WIP) and uses. Add architecture notes

--- Patterns_Of_Enterprise_Application_Patterns/PatternImplementations/DomanLogicPatterns/src/TransactionScript/RevenueRecognitionService.java
+++ Patterns_Of_Enterprise_Application_Patterns/PatternImplementations/DomanLogicPatterns/src/TransactionScript/RevenueRecognitionService.java
@@ -1,9 +1,9 @@
 package TransactionScript;
 
-import org.omg.CORBA.portable.ApplicationException;
+import commonImplementations.Money;
 
 import java.math.BigDecimal;
-import java.sql.Connection;
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -11,19 +11,48 @@ import java.sql.SQLException;
  * Created by Praise on 2017/02/15.
  */
 public class RevenueRecognitionService {
-        private Gateway db;
+    private Gateway db;
 
-        public Money recognizedRevenue(long contractNumber, MFDate asOf) {
-            Money result = Money.dollars(new BigDecimal(0));
-            try{
-                ResultSet rs = db.findRecognitionsFor(contractNumber, asOf);
-                while(rs.next()){
-                    result = result.add(Money.dollars(rs.getBigDecimal("amount")));
-                }
-                return result;
+    public RevenueRecognitionService(Gateway recognitionTableGateWay) {
+        this.db = recognitionTableGateWay;
+    }
+
+    //Sum up recognitions
+    public Money recognizedRevenue(long contractNumber, Date asOf) {
+        Money result = Money.dollars(new BigDecimal(0));
+        try {
+            ResultSet rs = db.findRecognitionsFor(contractNumber, asOf);
+            while (rs.next()) {
+                result = result.add(Money.dollars (rs.getBigDecimal("amount")));
             }
-            catch (SQLException e){
-                throw new Exception(e);
-            }
+            return result;
+        } catch (SQLException e) {
+            System.out.println(String.format("An error occured communicating with the database: %s", e.getMessage()));
         }
+        return result;
+    }
+
+    //S = Spreadsheet
+    //D = Database
+    //W = Word processor
+
+    public void calculateRevenueRecognitions(long contractNumber) {
+        try {
+            ResultSet contracts = db.findContract(contractNumber);
+            contracts.next();
+            Money totalRevenue  = Money.dollars(contracts.getBigDecimal("revenue"));
+            Date recoginitionDate = contracts.getDate("dateSinged");
+            String contractType = contracts.getString("type");
+
+            if(contractType.equals("S")){
+                Money [] allocation = totalRevenue.allocate(3);
+            }
+
+
+        } catch (SQLException e) {
+            System.out.println(String.format("An error occured communicating with the database: %s", e.getMessage()));
+        }
+    }
 }
+
+
