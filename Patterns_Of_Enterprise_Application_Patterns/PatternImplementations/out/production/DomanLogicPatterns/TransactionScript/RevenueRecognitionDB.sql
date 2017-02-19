debdf88e96c5145449ee91eb48f23ed189475e4e
Praise Tompane <tompanepp@gmail.com>
2017-02-19 13:50:31 +0200

    feature: update Money implementation(WIP) and uses. Add architecture notes

---
+++ Patterns_Of_Enterprise_Application_Patterns/PatternImplementations/out/production/DomanLogicPatterns/TransactionScript/RevenueRecognitionDB.sql
@@ -0,0 +1,16 @@
+
+
+CREATE TABLE products(ID int primary key,
+name varchar,
+type varchar)
+GO
+
+CREATE TABLE contracts(ID int primary key,
+product int foreign key references products(id),
+revenue decimal,
+dateSigned date)
+
+CREATE TABLE revenueRecognitions(contract int foreign key references contracts(id),
+amount decimal,
+recognizedOn date,
+PRIMARY KEY(contract ,recognizedOn))
