68b0aa05567ec017a58b7d3d7b6bff9bf20f8c20
Praise Tompane <tompanepp@gmail.com>
2017-02-25 21:21:52 +0200

    fix: update Proudct Type enum

---
+++ Patterns_Of_Enterprise_Application_Patterns/PatternImplementations/src/main/java/DomainLogicPatternsImplementations/RevenueRecognition/commonImplementations/ProductType.java
@@ -0,0 +1,21 @@
+package DomainLogicPatternsImplementations.RevenueRecognition.commonImplementations;
+
+/**
+ * Created by Praise on 2017/02/20.
+ */
+public enum ProductType {
+
+    Database ('D'),
+    Spreadsheet('S'),
+    WordProcessor('W');
+
+    private char value;
+
+    ProductType(char value) {
+        this.value = value;
+    }
+
+    public char getValue() {
+        return value;
+    }
+}
