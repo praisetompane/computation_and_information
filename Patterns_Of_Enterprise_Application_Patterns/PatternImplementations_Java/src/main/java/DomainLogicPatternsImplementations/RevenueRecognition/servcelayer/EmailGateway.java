7ddfbadf928fac6df3c7b74a9896dc704cc2dff6
Praise Tompane <tompanepp@gmail.com>
2017-03-07 21:22:05 +0200

    feature: partial implementation of table module

---
+++ Patterns_Of_Enterprise_Application_Patterns/PatternImplementations_Java/src/main/java/DomainLogicPatternsImplementations/RevenueRecognition/servcelayer/EmailGateway.java
@@ -0,0 +1,8 @@
+package DomainLogicPatternsImplementations.RevenueRecognition.servcelayer;
+
+/**
+ * Created by Praise on 2017/03/05.
+ */
+public interface EmailGateway {
+    void sendEmailMessage(String toAddress, String subject, String body);
+}
