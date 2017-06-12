e28c10293161b8185bbcc38838c67c69f1e54272
Praise Tompane <tompanepp@gmail.com>
2017-06-12 23:37:22 +0200

    fix: add routing, home and battle components

--- React Fundamentals/github-battle/app/components/App.js
+++ React Fundamentals/github-battle/app/components/App.js
@@ -1,5 +1,13 @@
 var React = require('react')
 var Popular = require('./Popular')
+var ReactRouter = require('react-router-dom')
+var Router = ReactRouter.BrowserRouter
+var Route = ReactRouter.Route
+var Switch = ReactRouter.Switch
+var Nav = require('./Nav')
+var Home = require('./Home')
+var Battle = require('./Battle')
+
 /*
     Properties:
         state
@@ -7,13 +15,24 @@ var Popular = require('./Popular')
         UI
 */
 //Just the component definition
+//className is for styling, we can't use 'class' because it;s reserved 
+//this is JSX, gets transpiled to normal JavaScript
+//<Switch>: switch match over the paths, if none is matched then render the 'defult' one = 'Not Found'
+//exact match exactly that path
 class App extends React.Component {
     render() {
         return (
-            //className is for styling, we can't use 'class' because it;s reserved 
-            <div className='container'> 
-                <Popular />
-            </div> //this is JSX, gets transpiled to normal JavaScript
+            <Router>
+                <div className='container'>
+                    <Nav />
+                    <Switch>
+                        <Route exact path='/' component={Home} />
+                        <Route path='/popular' component={Popular} />
+                        <Route exact path='/battle' component={Battle} />
+                        <Route render={() => <p>Not Found</p>} />
+                    </Switch>
+                </div>
+            </Router>
         )
     }
 }
