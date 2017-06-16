b39db359c5aa917cc9a25c63b33e614057269469
Praise Tompane <tompanepp@gmail.com>
2017-06-16 20:29:25 +0200

    fix: added player details to Battle page and routing

--- React Fundamentals/github-battle/app/utils/api.js
+++ React Fundamentals/github-battle/app/utils/api.js
@@ -1,9 +1,53 @@
 var axios = require('axios')
 
+var id = "YOUR_CLIENT_ID"
+var sec = "YOUR_SECRET_ID"
+var params = "?client_id=" + id + "&client_secret=" + sec
+
+var baseURL = 'https://api.github.com'
+
+function getProfile(username) {
+    return axios.get(baseURL + '/users/' + username + params)
+        .then((user) => { return user.data })
+}
+
+function getRepos(username) {
+    return axios.get(baseURL + '/users/' + username + '/repos' + params + '&per_page=100')
+    //.then((repos) => { return repos.data })
+}
+
+function getStartCount(repos) {
+    return repos.data.reduce(
+        (count, repo) => { return count + repo.stargazers_count },
+        0)
+}
+
+function calculateScore(profile, repos) {
+    var followers = profile.followers
+    var totalStars = getStartCount(repos)
+
+    return (followers * 3) + totalStars
+}
+
+
+function handleError(erroe) {
+    console.warn(error)
+    return null
+}
+
+function getUserData(player) {
+    return axios.all([]).then()
+
+}
 module.exports = {
+
+    battle: function (players) {
+
+    },
+
     fetchPopularRepos: function (language) {
         var encodedURI = window.encodeURI(
-            'https://api.github.com/search/repositories?q=stars:>1+language:' + language + '&sort=stars&order=desc&type=Repositories');
+            baseURL + '/search/repositories?q=stars:>1+language:' + language + '&sort=stars&order=desc&type=Repositories');
 
         return axios.get(encodedURI).then((response) => response.data.items)
     }
