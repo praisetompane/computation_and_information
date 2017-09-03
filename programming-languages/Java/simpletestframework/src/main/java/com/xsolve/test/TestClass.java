d41b288169a733f0ad2f1171f8af22ade4340eb7
Praise Tompane <praise.tompane@allangray.co.za>
2017-09-03 20:50:36 +0200

    fix: learning java annotation by building a test framework

---
+++ programming-languages/Java/simpletestframework/src/main/java/com/xsolve/test/TestClass.java
@@ -0,0 +1,15 @@
+package com.xsolve.test;
+
+import com.xsolve.test.core.TesterInfo;
+
+/**
+ * Created by vagrant on 2017/09/03.
+ */
+
+@TesterInfo(
+        priority = TesterInfo.Priority.HIGH,
+        createdBy = "praise",
+        tags = {"sales", "test"}
+)
+public class TestExmple {
+}
