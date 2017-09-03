a9275b19d9f0a7545d7ed5ed7722259a0d51d9f7
Tompanepp <tompanepp@gmail.com>
2017-09-03 20:52:18 +0200

    fix: learning java annotation by building a test framework

---
+++ frameworks-and-libraries/React/github-battle/app/index.js
@@ -0,0 +1,9 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('./index.css') //to include our custom css when we bundle the code
+var App = require('./components/App')
+
+//Actaully display on the DON
+ ReactDOM.render(<App />, //component to render 
+                 document.getElementById('app') //element in the index.html to render the component onto
+                );
