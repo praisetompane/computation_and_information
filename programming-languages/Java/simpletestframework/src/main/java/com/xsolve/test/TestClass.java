b538d1e43448ded5c4ec72fd51d76ff492a5d244
Tompanepp <tompanepp@gmail.com>
2017-09-03 21:19:03 +0200

    fix: learning java annotation by building a test framework

--- programming-languages/Java/simpletestframework/src/main/java/com/xsolve/test/TestClass.java
+++ programming-languages/Java/simpletestframework/src/main/java/com/xsolve/test/TestClass.java
@@ -1,5 +1,6 @@
 package com.xsolve.test;
 
+import com.xsolve.test.core.Test;
 import com.xsolve.test.core.TesterInfo;
 
 /**
@@ -11,5 +12,23 @@ import com.xsolve.test.core.TesterInfo;
         createdBy = "praise",
         tags = {"sales", "test"}
 )
-public class TestExmple {
+public class TestClass {
+
+    @Test
+    void testA() {
+        if (true)
+            throw new RuntimeException("This test always fails");
+    }
+
+    @Test(enabled = false)
+    void testB() {
+        if (false)
+            throw new RuntimeException("This test always passes");
+    }
+
+    void testC() {
+        if (10 > 1) {
+            //do nothing, this test always passes
+        }
+    }
 }
