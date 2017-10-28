71df3d00e1803004f98170f72d66bcde530dc388
Praise Tompane <tompanepp@gmail.com>
2017-10-28 19:01:43 +0200

    fix: refactored the Leibniz implementation and used that base to implement the Madhava approximation of pi. This is is why I love recursive solutions. You sort encode the infrastructure and only the piece where the actual work is being done is updated, (i.e. line 10)

--- programming-languages/Python/MadhavaPi.py
+++ programming-languages/Python/MadhavaPi.py
@@ -1,10 +1,13 @@
-#approximation of pi using Leibniz approxiation
-#an infinite series expansion
-#pi/4 = 1 - 1/3 + 1/5 - 1/7 + 1/9 .....
+import math
+#approximation of pi using Madhava approxiation
+#expansion using first 21 terms
+#pi = squareroot(12)(1 - 1/3*3 + 1/5*3^2 - 1/7*3^3 + )
 
 def myPi(iterations):
     def approximate(currentIteration, piApproximation, denominator):
-        nextSeriesNumber = 1/denominator
+        '''made this mistake that cost me some debugging: 'nextSeriesNumber = 1/ denominator * (3 ** currentIteration)' 
+        since division has a higher precedence my pi was sooooo wrong, try it and see for yoursefl '''
+        nextSeriesNumber = 1/(denominator * (3 ** currentIteration))
         if currentIteration == iterations: return piApproximation
         else:
             nextDenomiator = denominator + 2
@@ -14,9 +17,10 @@ def myPi(iterations):
             else:
                 return approximate(nextIteration, piApproximation - nextSeriesNumber, nextDenomiator)
 
-    return approximate(1, 1, 3) * 4
+    app = approximate(1, 1, 3)
+    return app * math.sqrt(12)
 
 def main():
-    print('myPi', myPi(500))
+    print('myPi', myPi(21))
 
 main()
