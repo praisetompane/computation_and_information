2874f30f5b1efa8f7ce1e95f75b536841a6c625f
Praise Tompane <tompanepp@gmail.com>
2017-10-28 22:27:56 +0200

    fix: a general implementation of a function to compute a pi approximation using a pluggable approximation method, (thank you Lambda calculus for Functional programming)

--- programming-languages/Python/LeibnizPi.py
+++ programming-languages/Python/LeibnizPi.py
@@ -3,18 +3,16 @@
 #pi/4 = 1 - 1/3 + 1/5 - 1/7 + 1/9 .....
 
 def myPi(iterations):
-    def approximate(currentIteration, piApproximation, denominator):
-        nextSeriesNumber = 1/denominator
+    def approximate(currentIteration, piApproximation, denominator, sign):
+        nextSeriesNumber = 1/denominator * sign
         if currentIteration == iterations: return piApproximation
         else:
             nextDenomiator = denominator + 2
             nextIteration = currentIteration + 1
-            if currentIteration % 2 == 0: 
-                return approximate(nextIteration, piApproximation + nextSeriesNumber, nextDenomiator)
-            else:
-                return approximate(nextIteration, piApproximation - nextSeriesNumber, nextDenomiator)
+            return approximate(nextIteration, piApproximation + nextSeriesNumber, nextDenomiator, sign * -1)
 
-    return approximate(1, 1, 3) * 4
+
+    return approximate(1, 1, 3, -1) * 4
 
 def main():
     print('myPi', myPi(500))
