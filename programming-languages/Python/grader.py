6cecf5c1a292a3da41dfe2dc47917f80262ebc85
Praise Tompane <tompanepp@gmail.com>
2017-11-05 20:55:51 +0200

    fix: grader simplified

--- programming-languages/Python/grader.py
+++ programming-languages/Python/grader.py
@@ -1,12 +1,12 @@
 def getGrade(grade):
-    if grade >= 90: print('A')
-    elif grade >= 80 and grade < 90: print('B')
-    elif grade >= 70 and grade < 80: print('C')
-    elif grade >= 60 and grade < 70: print('D')
-    else: print('F')
+    if grade >= 90: return 'A'
+    elif grade >= 80: return 'B'
+    elif grade >= 70: return 'C'
+    elif grade >= 60: return 'D'
+    else: return('F')
 
-getGrade(100)
-getGrade(90)
-getGrade(82)
-getGrade(78)
-getGrade(60)
+print(getGrade(100))
+print(getGrade(90))
+print(getGrade(82))
+print(getGrade(78))
+print(getGrade(60))
