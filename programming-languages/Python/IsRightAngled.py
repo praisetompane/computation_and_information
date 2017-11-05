260ea3a466d6c8da51b985d4eff044b15a6ddd0a
Praise Tompane <tompanepp@gmail.com>
2017-11-06 00:22:25 +0200

    fix: improved isRightAngled

--- programming-languages/Python/IsRightAngled.py
+++ programming-languages/Python/IsRightAngled.py
@@ -1,10 +1,18 @@
 from math import sqrt
 
 def is_rightangled(a, b, c):
-    return approximatelyEqual(c, sqrt(a**2 + b**2))
+    if c > a and c > b:
+        return isApproximatelyEqual(c, sqaureRootOfSumOfSides(a, b))
+    elif b > a and b > c:
+        return isApproximatelyEqual(b, sqaureRootOfSumOfSides(a, c))
+    else:
+        return isApproximatelyEqual(a, sqaureRootOfSumOfSides(b, c))
 
-def approximatelyEqual(a, b):
-    return abs(a - b) < 0.001
+def sqaureRootOfSumOfSides(a, b):
+    return sqrt(a**2 + b**2)
+    
+def isApproximatelyEqual(a, b):
+    return abs(a - b) < 0.0001
 
 def main():
     print(is_rightangled(3,4,5))
