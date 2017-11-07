6031584da250b21755538340abe09f4815533a9e
Praise Tompane <tompanepp@gmail.com>
2017-11-07 02:26:08 +0200

    fix: improved naming and made the use of math.sqrt more explicit to simplify the sumSquaredSides implementation

--- programming-languages/Python/IsRightAngled.py
+++ programming-languages/Python/IsRightAngled.py
@@ -2,19 +2,20 @@ from math import sqrt
 
 def is_rightangled(a, b, c):
     if c > a and c > b:
-        return isApproximatelyEqual(c, sqaureRootOfSumOfSides(a, b))
+        return isApproximatelyEqual(c, sqrt(sumSquaredSides(a, b)))
     elif b > a and b > c:
-        return isApproximatelyEqual(b, sqaureRootOfSumOfSides(a, c))
+        return isApproximatelyEqual(b, sqrt(sumSquaredSides(a, c)))
     else:
-        return isApproximatelyEqual(a, sqaureRootOfSumOfSides(b, c))
+        return isApproximatelyEqual(a, sqrt(sumSquaredSides(b, c)))
 
-def sqaureRootOfSumOfSides(a, b):
-    return sqrt(a**2 + b**2)
+def sumSquaredSides(a, b):
+    return a**2 + b**2
     
 def isApproximatelyEqual(a, b):
     return abs(a - b) < 0.0001
 
 def main():
     print(is_rightangled(3,4,5))
+
 
 main()
