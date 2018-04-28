d4f3a5f939ecce76fccbbd510e93bfa43aa0ec66
Praise Tompane <tompanepp@gmail.com>
2018-04-28 19:20:59 +0200

    fix: improve implementation of Show type class to remove cumbersome usage

--- programming-languages/Scala/TypeClasses.scala
+++ programming-languages/Scala/TypeClasses.scala
@@ -7,20 +7,28 @@ trait Show[A] {
 }
 
 //Mine
-object ShowInt {
+object ShowMine {
+    //Limitation: Doesn't allow specific types to define their own way of being shown :(
+    def show[A](a: A): String = s"$a"
+
     def showInt: Show[Int] =  
         new Show[Int] {
             def show[Int](a: Int): String = s"int $a" 
         }
 }
 
-println(ShowInt.showInt.show(1))
-println("Apl")
+//cumbersome usage
+println(ShowMine.showInt.show(1))
+println(ShowMine.show(1))
+
 
 //Theirs
 
 object Show {
-    val intCanShow: Show[Int] = 
+    //Note use to implicits compared to mine
+    def show[A](a: A)(implicit sh: Show[A]):String =  sh.show(a)
+
+    implicit val intCanShow: Show[Int] = 
         new Show[Int] {
             def show[Int](int: Int): String = s"int $int"
         }
@@ -28,3 +36,4 @@ object Show {
 
 //cumbersome usage
 println(Show.intCanShow.show(1))
+println(Show.show(1))
