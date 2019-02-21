e01e4f1726d03581f4f5348451eecf2c3460c2d3
Praise Tompane <tompanepp@gmail.com>
2019-02-21 19:21:31 +0200

    fix: improvement on the parenthesis validator for more clarity

--- algorithms-data-structures/stack-paranthesis-linter.js
+++ algorithms-data-structures/stack-paranthesis-linter.js
@@ -1,25 +1,41 @@
-function bracketsMatch(characters) {
+circleOpeningBracket = '('
+circleClosingBracket = ')'
+curlyOpeningBracket = '{'
+curlyClosingBracket = '}'
+squareOpeningBracket = '['
+squareClosingBracket = ']'
+
+const bracketsMatch = (code) => {
+    characters = code.split('')
     //stack in name for learning
     openingBracketStack = []
-    circleOpeningBracket = '('
-    curlyOpeningBracket = '{'
-    squareOpeningBracket = '['
     for (i = 0; i < characters.length; i++) {
-        bracket = characters[i]
-        if (bracket == circleOpeningBracket || bracket == curlyOpeningBracket || bracket == squareOpeningBracket) {
-            openingBracketStack.push(bracket)
-        } else if (bracket == ')' && openingBracketStack.pop() != circleOpeningBracket) {
-            return false;
-        } else if (bracket == ']' && openingBracketStack.pop() != squareOpeningBracket) {
-            return false;
-        } else if (bracket == '}' && openingBracketStack.pop() != curlyOpeningBracket) {
-            return false;
+        character = characters[i]
+        if (isOpeningBrace(character)) {
+            openingBracketStack.push(character)
+        } else if (isClosingBrace(character)) {
+            if (!isClosingBracketOfRecentOpeningBracket(character, openingBracketStack)) {
+                return false
+            }
         }
     }
-    return openingBracketStack == 0;
+    return openingBracketStack.length == 0;
 }
 
-console.log('Should be invalid: is valid', bracketsMatch(['(', ',', '(', ')', '}']))
-console.log('Should be valid: is valid', bracketsMatch(['[', '(', '{', '}', ')', 't', ']']))
-console.log('Should be invalid: is valid', bracketsMatch(['}']))
-console.log('Should be invalid: is valid', bracketsMatch(['[', '[', '(', '{', '}', ')', 't', ']']))
+const isClosingBracketOfRecentOpeningBracket = (character, openingBracketStack) => {
+    const openingBracket = {
+        circleClosingBracket: circleOpeningBracket,
+        curlyClosingBracket: curlyOpeningBracket,
+        squareClosingBracket: squareOpeningBracket
+    }
+    return openingBracket[character] == openingBracketStack.pop
+}
+
+const isOpeningBrace = (character) => [circleOpeningBracket, curlyOpeningBracket, squareOpeningBracket].includes(character)
+
+const isClosingBrace = (character) => [circleClosingBracket, curlyClosingBracket, squareClosingBracket].includes(character)
+
+console.log('Should be invalid: is valid', bracketsMatch('[(,()}])'))
+console.log('Should be valid: is valid', bracketsMatch('[[({})t]]'))
+console.log('Should be invalid: is valid', bracketsMatch('[}])'))
+console.log('Should be invalid: is valid', bracketsMatch('[[[({})t]])'))
