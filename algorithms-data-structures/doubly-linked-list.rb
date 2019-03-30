e634eba9cadeddf72815063fd3b50cba6425bfd5
Praise Tompane <tompanepp@gmail.com>
2019-03-30 23:56:15 +0200

    fix: implelment queue using DoublyLinkedList

--- algorithms-data-structures/doubly-linked-list.rb
+++ algorithms-data-structures/doubly-linked-list.rb
@@ -6,6 +6,15 @@ class Node
     end
 end
 
+'''
+    Since DoublyLinkedList knows first and last node
+    insertion at end is O(1) 
+    removal from beginning is O(1)
+
+    perfect for a queue
+    vs normal linked list O(1) removal from beginning, O(N) insertion at the end
+    vs array O(N) removel from beginning, O(1) insetion at the end
+'''
 class DoublyLinkedList
     attr_accessor :first_node, :last_node
     def initialize(first_node = nil, last_node = nil)
@@ -24,7 +33,13 @@ class DoublyLinkedList
         end
     end
 
-    def print()
+    def remove_front_front
+        _first_node = @first_node
+        @first_node = @first_node.previous_node
+        return _first_node    
+    end
+
+    def print
         _print(@first_node)
     end
     private def _print(current_node)
@@ -36,10 +51,29 @@ class DoublyLinkedList
     end
 end
 
+class Queue
+    attr_accessor :data
+
+    def initialize
+       @data = DoublyLinkedList.new() 
+    end
+    
+    def enqueue(value)
+        return @data.insert_at_end(value)
+    end
+
+    def dequeue
+        @data.remove_front_front
+    end
+
+    def print
+        @data.print()
+    end
+end
 
 #Testing
 
-#usage
+puts "uasge of Doubly Linked List direct"
 node1 = Node.new(1)
 #"linking" node1 to node2
 node2 = Node.new(2)
@@ -51,9 +85,29 @@ node2.next_node = node3
 node4 = Node.new(4)
 node3.next_node = node4
 
-linkedlist = DoublyLinkedList.new()
+linkedlist = DoublyLinkedList.new(node1, node4)
 linkedlist.print()
 
 puts "inserting at the end"
 linkedlist.insert_at_end(5)
 linkedlist.print()
+
+puts "removing from front"
+front_node = linkedlist.remove_front_front()
+puts "front node data #{front_node.data}"
+
+
+puts "Doubly linked list as perfect basis for queue"
+
+shopping_queue = Queue.new()
+puts "shoopers comes along"
+shopping_queue.enqueue("Sue")
+shopping_queue.enqueue("Thapelo")
+shopping_queue.enqueue("Mashoto")
+
+puts "printing shoopers"
+shopping_queue.print()
+
+puts "chasier gets to work"
+shopper = shopping_queue.dequeue()
+puts "first shopper assisted #{shopper.data}" 
