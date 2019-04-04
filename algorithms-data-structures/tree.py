2e41f38a864a1b0acbde94257d52193430c11c64
Praise Tompane <tompanepp@gmail.com>
2019-04-04 22:05:13 +0200

    fix: implement insert and print rint graphical tree

--- algorithms-data-structures/tree.py
+++ algorithms-data-structures/tree.py
@@ -23,6 +23,40 @@ class BinaryTree:
             elif value > node.val: return _search(node._rightchild)
         return _search(self._root)
 
+    def insert(self, value):
+        def _insert(node):
+            if value < node.val and not node._leftchild: 
+                node.leftchild = node
+            else:
+                _insert(node._leftchild)
+            
+            if value > node.val and not node._rightchild: 
+                node._rightchild = node
+            else:
+                _insert(node._rightchild)
+        _insert(self._root)
+
+    def print_tree(self):
+        print self._root.val
+        def _print(node):
+            if node: 
+                if node._leftchild and node._rightchild: 
+                    print '/',
+                    print '\\'
+                    print node._leftchild.val,
+                    print node._rightchild.val
+                    _print(node._leftchild)
+                    _print(node._rightchild)
+                if node._leftchild and not node._rightchild:
+                    print '/'
+                    print node._leftchild.val
+                    _print(node._leftchild)
+                if node._rightchild and not node._leftchild:
+                    print '\\'
+                    print node._rightchild.val
+                    _print(node._rightchild)
+        _print(self._root)
+
 node52 = Node(52)
 node61 = Node(61)
 node56 = Node(56, node52, node61)
@@ -41,7 +75,14 @@ node10 = Node(10, node4, node11)
 
 node25 = Node(25, node10, node33)
 node75 = Node(75, node56, node89)
-root = Node(50, node25, node25)
+root = Node(50, node25, node75)
 
 tree = BinaryTree(root)
-print tree.search(11).val
+print "searching for the value 11"
+print tree.search(50).val
+
+print "print the tree"
+tree.print_tree()
+
+tree.insert
+
