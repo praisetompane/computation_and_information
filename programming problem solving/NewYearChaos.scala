1700c59d43d6cf8cfc250bc0c4a6454f82acc50f
Tompanepp <tompanepp@gmail.com>
2020-07-10 22:11:19 +0200

    bubble sort used to detect validity of a shuffled state with a constraint of two max forward swaps

--- programming problem solving/NewYearChaos.scala
+++ programming problem solving/NewYearChaos.scala
@@ -39,24 +39,53 @@
             else bribes += bribesUsed
 */
 
+/*
+    Ascending bubble sort
+
+    if(index == arr.length) finish
+    if(value > nextValue)
+        swap(value, nextValue)
+        increment bribes
+
+
+    Any person in the queue can bribe the person directly in front of them to swap positions.
+    One person can bribe at most two others
+*/
+
 def minimumBribes(finalQueue: Array[Int]): Unit = {
     val MaxBribes = 2
+    val finalIndex = finalQueue.length - 1
     var bribes = 0
+    var swapped = true
+    val zeroIndexOffset = 1
 
-    //PROBLEM: double counting
-    finalQueue.zipWithIndex.foreach {
-        case(value: Int, index: Int) => 
-            val zeroIndexOffset = 1
-            val bribesUsed = math.abs(value - index - zeroIndexOffset)
-            //println(s"bribesUsed $bribesUsed for value $value")
-            //assumes once bribed backwards,you won't bribe forwards
-            if(value > index) {
-                if(bribesUsed > MaxBribes) {
+    def swap(indexA: Int, indexB: Int) = {
+        val tempB = finalQueue(indexB)
+        finalQueue(indexB) = finalQueue(indexA)
+        finalQueue(indexA) = tempB
+    }
+
+    while(swapped) {
+        swapped = false
+        finalQueue.zipWithIndex.foreach {
+            case(value: Int, index: Int) => 
+                val nextIndex = index + 1
+                val distanceFromOriginalPosition = math.abs(value - (index + zeroIndexOffset))
+
+                if(value > index && distanceFromOriginalPosition > MaxBribes) {
                     println("Too chaotic")
                     return
                 }
-                else bribes += bribesUsed
-            }
+
+                if(index != finalIndex && value > finalQueue(nextIndex)) {
+                    swapped = true
+                    bribes += 1
+                    swap(index, nextIndex)
+                }
+        }
     }
+
     println(bribes)
 }
+
+minimumBribes(Array(1,2,5,3,7,8,6,4))
