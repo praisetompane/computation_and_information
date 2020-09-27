61a8d0d08338187f63df2badcfe3487d22ee484a
Tompanepp <tompanepp@gmail.com>
2020-09-27 06:16:45 +0200

    rearrange folders to represent current models of modern computing with some lazy folders at the end

--- books/Patterns_Of_Enterprise_Application_Patterns/PatternImplementations_Java/src/main/java/datasourcearchitecturalpatterns/datamapper/StatementSource.java
+++
@@ -1,9 +0,0 @@
-package datasourcearchitecturalpatterns.datamapper;
-
-/**
- * Created by Praise on 2017/03/12.
- */
-public interface StatementSource {
-    String sql();
-    Object[] parameters();
-}
