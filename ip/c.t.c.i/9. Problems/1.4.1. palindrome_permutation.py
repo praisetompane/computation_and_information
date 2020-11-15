0d7ee64971c73fcb0d2a3cca7dd563a116d481a8
Tompanepp <tompanepp@gmail.com>
2020-11-15 13:55:00 +0200

    standard input and output in C

--- ip/c.t.c.i/9. Problems/1.4.1. palindrome_permutation.py
+++ ip/c.t.c.i/9. Problems/1.4.1. palindrome_permutation.py
@@ -18,14 +18,8 @@
                     backwards AND forwards
 
     algo flow:
-        generate list of palindromes possible from input
-            check if permutation of input is in list
-
-        refinement:
-            since there is only one string,we can conclude that the possible palindromes generated are permutations of it.
-
-            The core proble is to:
-                find a palindrome using the characters of the string
+        check if string is a palindrome
+        if it is a palindrome, then it IS a permutation of palindrome 
 
 '''
 from itertools import permutations
@@ -35,26 +29,37 @@ def is_palindrome_permutation(string_input):
         #check if at least one is a palindrome
             #if there are no palindromes for the characters of string_input
                 #then string_input is not a permutation of a palindrome
-
+    string_input = string_input.lower()
     def is_palindrome(permutation):
         #generate reversed vrersion of the permutation
         #compare reversed version characters from the END
             #with the original version characters from the BEGINNING
-        string_reversed = permutation[::-1] #means start at end of string an stop and start of the string
-        string_length = len(string_reversed)
-        for i in range(string_length - 1):
-            if string_reversed[string_length - i - 1] != string_input[i]: return False
-        return True
+        permutation_reversed = permutation[::-1] #means start at end of string an stop and start of the string
+        if(permutation == 'taco cat'):
+            print('string reverse', permutation_reversed)
+            string_length = len(permutation_reversed)
+            for i in range(string_length - 1):
+                print('compare chars',permutation_reversed[string_length - i - 1], string_input[i]  )
+                if permutation_reversed[string_length - i - 1] != string_input[i]: return False
+            return True
 
     for p in permutations(string_input):
-        permutation = ''.join(p) 
-        if(is_palindrome(permutation)): return True
+        permutation = ''.join(p).lower() 
+        if(permutation == 'taco cat'): print('Found taco cat')
+        if(is_palindrome(permutation)): 
+            print('palindromic permuation', permutation)
+            return True
     return False
 
 print(is_palindrome_permutation('Tact Coa')) #true
+'''
 print(is_palindrome_permutation('act TCoa')) #true
 print(is_palindrome_permutation('A but tuba')) #true
 print(is_palindrome_permutation('ut tu Ab ba')) #true
+print(is_palindrome_permutation('u')) #true
+print(is_palindrome_permutation('')) #true
+print(is_palindrome_permutation('peter')) #false
+'''
 
 
 '''
