ee0a1c0ba5d025201110b54d93705d65e1414a93
Tompanepp <tompanepp@gmail.com>
2021-01-12 23:01:18 +0200

    basic computation with variables and a while loop

---
+++ 2. programming-languages/base-languages/C/the-c-programming-language/0. tutorial/1.variables-and-arithmetic.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/* print Fahrenheit-Celsius table for fahr = 0, 20, ..., 300 */
+main() {
+    int fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+    printf("fahr \t celsius\n");
+    while(fahr <= upper) {
+        celsius = (5 * (fahr - 32)) / 9;
+        printf("%d\t%d\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
