779a9226308359cad7b7bf0d630a2b3299dfd315
Tompanepp <tompanepp@gmail.com>
2021-01-12 23:29:47 +0200

    use floating-point arithmetic for more accurate results

--- 2. programming-languages/base-languages/C/the-c-programming-language/0. tutorial/1.variables-and-arithmetic.c
+++ 2. programming-languages/base-languages/C/the-c-programming-language/0. tutorial/1.variables-and-arithmetic.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /* print Fahrenheit-Celsius table for fahr = 0, 20, ..., 300 */
 main() {
-    int fahr, celsius;
-    int lower, upper, step;
+    float fahr, celsius;
+    float lower, upper, step;
 
     lower = 0;
     upper = 300;
@@ -11,8 +11,8 @@ main() {
     fahr = lower;
     printf("fahr \t celsius\n");
     while(fahr <= upper) {
-        celsius = (5 * (fahr - 32)) / 9;
-        printf("%3d\t%6d\n", fahr, celsius);
+        celsius = (5.0/9.0) * fahr - 32.0;
+        printf("%3.0f\t%6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
