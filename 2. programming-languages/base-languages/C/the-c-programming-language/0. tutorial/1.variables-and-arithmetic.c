315f4a2fbc0fd1a9870c11071b5216fbbffb8c99
Tompanepp <tompanepp@gmail.com>
2021-01-12 23:02:20 +0200

    adding a width to right justify output

--- 2. programming-languages/base-languages/C/the-c-programming-language/0. tutorial/1.variables-and-arithmetic.c
+++ 2. programming-languages/base-languages/C/the-c-programming-language/0. tutorial/1.variables-and-arithmetic.c
@@ -12,7 +12,7 @@ main() {
     printf("fahr \t celsius\n");
     while(fahr <= upper) {
         celsius = (5 * (fahr - 32)) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        printf("%3d\t%6d\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
