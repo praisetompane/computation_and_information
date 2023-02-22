8ffe04cffbf3ec764b808f04b7c7146a3144fd00
Praise Tompane <praisetompane@icloud.com>
2023-02-22 23:36:23 +0200

    move to asdf to managing scala development environment

--- 0_theoretical_computer_science/3_programming_language_theory_and_formal_methods/1_languages/scala/README.md
+++ 0_theoretical_computer_science/3_programming_language_theory_and_formal_methods/1_languages/scala/README.md
@@ -1,7 +1,7 @@
 # Playground to experiment with Scala
 # sources
-* library reference: https://www.scala-lang.org/api/3.2.2/
-* language reference: https://scala-lang.org/
+* library reference: https://scala-lang.org/api/3.x/
+* language reference: https://docs.scala-lang.org/scala3/reference/
 * module index: https://index.scala-lang.org/
 * extending and embedding:
 
--- 0_theoretical_computer_science/3_programming_language_theory_and_formal_methods/1_languages/scala/setup.sh
+++ 0_theoretical_computer_science/3_programming_language_theory_and_formal_methods/1_languages/scala/setup.sh
@@ -1,7 +1,23 @@
-echo "installing system wide scala"
-brew install scala
+echo "adding asdf scala plugin"
+asdf plugin add scala
+echo "done"
+
+echo "installing scala"
+asdf install scala latest
+echo "done"
+
+echo "set latest version to system wide version"
+asdf global scala latest
+echo "done"
+
+echo "adding asdf sbt plugin"
+asdf plugin add sbt
 echo "done"
 
 echo "installing sbt"
-brew install sbt
+asdf install sbt latest
+echo "done"
+
+echo "set latest version to system wide version"
+asdf global sbt latest
 echo "done"
