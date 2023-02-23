dd0cba3904447ee229bd767de204b0ddb2decf48
Praise Tompane <praisetompane@icloud.com>
2023-02-23 02:33:00 +0200

    filtering operations on List type. move project to scala 3 and latest sbt

---
+++ 0_theoretical_computer_science/3_programming_language_theory_and_formal_methods/1_languages/scala/src/main/scala/5_collections_list_filter_positions.scala
@@ -0,0 +1,11 @@
+def filter_out_odd_indices(arr: List[Int]): List[Int] = {
+  def is_odd(number: Int) = number % 2 == 1
+  arr.zipWithIndex
+    .filterNot { (value, index) => is_odd(index + 1) }
+    .map { (value, index) => value }
+}
+
+@main def main() = {
+  val numbers = List(2, 5, 3, 4, 6, 7, 9, 8)
+  println(f(numbers))
+}
