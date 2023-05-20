899e4b45e1186658e2d1fc484b03cce5e22efbe8
Praise Tompane <praisetompane@icloud.com>
2023-05-20 18:01:42 +0200

    git global ignores

---
+++ 5_software_and_its_engineering/1_software_notations_and_tools/6_software_configuration_management_and_version_control_systems/git/setup.sh
@@ -0,0 +1,3 @@
+# setup git global ignore
+# useful for system wide no repo specific ignore
+git config --global core.excludesfile /Users/praisetompane/Library/Mobile\ Documents/com~apple~CloudDocs/global_config/.gitignore
