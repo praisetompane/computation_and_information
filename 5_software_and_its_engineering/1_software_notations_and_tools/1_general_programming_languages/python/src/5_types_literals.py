47ab84a13d0c1b02b6f732e1d1d722a4e9fcafad
Praise Tompane <praisetompane@icloud.com>
2023-06-27 17:49:20 +0200

    docs(python):literals
    add docs for string and bytes literals

---
+++ 5_software_and_its_engineering/1_software_notations_and_tools/1_general_programming_languages/python/src/5_types_literals.py
@@ -0,0 +1,15 @@
+a_string_literal = "foo"
+print(a_string_literal)
+
+a_string_literal = """foo"""
+print(a_string_literal)
+
+a_string_literal = 'foo'
+print(a_string_literal)
+
+a_string_literal = '''foo'''
+print(a_string_literal)
+
+
+a_bytes_literal = b"00000000"
+print(a_bytes_literal)
