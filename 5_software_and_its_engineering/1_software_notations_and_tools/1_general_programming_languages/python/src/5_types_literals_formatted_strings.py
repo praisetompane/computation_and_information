688149e0394c1a2e08c5cc614a0617e6f608f548
Praise Tompane <praisetompane@icloud.com>
2023-07-08 18:22:45 +0200

    docs(python/1_tokens)
    format string properties, modifiers

--- 5_software_and_its_engineering/1_software_notations_and_tools/1_general_programming_languages/python/src/5_types_literals_formatted_strings.py
+++ 5_software_and_its_engineering/1_software_notations_and_tools/1_general_programming_languages/python/src/5_types_literals_formatted_strings.py
@@ -1,14 +1,30 @@
+from datetime import date
+
 string_variable = "i am a variable"
 numeric_variable = 1
 
 a_formatted_string_literal = f"a formatted with a {string_variable}"
 print(a_formatted_string_literal)
+print('\n')
 
 a_formatted_string_literal = f"a formatted with a {numeric_variable}"
 print(a_formatted_string_literal)
+print('\n')
 
 a_string_with_double_curly_brace = f"a formatted with a {{ numeric_variable }}"
 print(a_string_with_double_curly_brace)
+print('\n')
 
-a_formatted_string_literal = f"a formatted with a {string_variable}"
-print(a_formatted_string_literal)
+print("example using a conversion modifier '!r'. notice the "''" around the value showing it is a string")
+a_formatted_string_with_conversion_field = f"a formatted with a {string_variable!r}"
+print(a_formatted_string_with_conversion_field)
+print('\n')
+
+print("example using a format spec ':'")
+today = date(year=2023, month=1, day=27)
+a_formatted_string_with_format_modifier = f"today's date is {today:%Y %B %d}"
+print(a_formatted_string_with_format_modifier)
+print('\n')
+
+
+
