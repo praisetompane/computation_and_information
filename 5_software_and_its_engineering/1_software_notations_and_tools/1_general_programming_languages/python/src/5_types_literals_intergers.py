775c050d2835315e0ec5e3233bb3eb128b3082a4
Praise Tompane <praisetompane@icloud.com>
2023-07-13 16:56:36 +0200

    docs(python/1_tokens)
    add integer literals

---
+++ 5_software_and_its_engineering/1_software_notations_and_tools/1_general_programming_languages/python/src/5_types_literals_intergers.py
@@ -0,0 +1,28 @@
+a_number = 1
+print(a_number)
+
+# '-' is a unary operator, which negates '1'
+a_negative_number = -1
+print(a_negative_number)
+
+an_underscored_number = 100_000_000
+print(an_underscored_number)
+print(an_underscored_number + 1)
+
+binary_integer_3 = 0b0000_0011
+print(binary_integer_3)
+
+binary_integer_1 = 0b00000001
+print(binary_integer_1)
+
+octal_integer_79 = 0o117
+print(octal_integer_79)
+
+octal_integer_15 = 0o01_7
+print(octal_integer_15)
+
+hexadecimal_integer = 0xdeadbeef
+print(hexadecimal_integer)
+
+hexadecimal_integer_144 = 0x9_0
+print(hexadecimal_integer_144)
