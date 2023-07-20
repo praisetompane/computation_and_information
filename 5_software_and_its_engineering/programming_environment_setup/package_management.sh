264f8452872b896248a755ebdb484063fa79f889
Praise Tompane <praisetompane@icloud.com>
2023-07-20 23:02:39 +0200

    fix(programming_environment_setup)
    add separate installation path for linux

--- 5_software_and_its_engineering/programming_environment_setup/package_management.sh
+++ 5_software_and_its_engineering/programming_environment_setup/package_management.sh
@@ -1,3 +1,15 @@
 echo "installing brew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo "done"
+
+
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/praisetompane/.profile
+
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+ostype="$(uname -s)"
+if [ "$ostype" = "Linux" ]; then 
+    sudo apt-get install build-essential
+fi
+
+brew install gccac
