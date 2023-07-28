a7f951e0424158122d825570fc7767db3d8296b9
Praise Tompane <praisetompane@icloud.com>
2023-07-28 17:10:03 +0200

    docs(2_hardware):
    break up components for easier comprehension. simmplied flow for how a computer starts 'boots up'. add general machine setup to soruce version control.

---
+++ general_machine_setup.sh
@@ -0,0 +1,28 @@
+echo "installing pytube"
+    # source: https://github.com/pytube/pytube#
+    # download: pytube {url}
+    python -m pip install pytube
+echo "done"
+
+echo "installing zoom"
+    brew install zoom
+echo "done"
+
+echo "installing discord"
+    brew install discord
+echo "done"
+
+echo "installing etcher"
+    brew install balenaetcher
+echo "done" 
+
+echo "installing mactex"
+    brew install mactex
+echo "done" 
+
+#echo "installing scarlet 2i2 studio 3rd generation"
+#filename="focusrite.dmg" 
+#curl -o focusrite.dmg https://fael-downloads-prod.focusrite.com/customer/prod/downloads/Focusrite%20Control%20-%203.11.0.1983.dmg
+#sudo hdiutil attach $filename
+#sudo installer -package /Volumes/
+#echo "done"
