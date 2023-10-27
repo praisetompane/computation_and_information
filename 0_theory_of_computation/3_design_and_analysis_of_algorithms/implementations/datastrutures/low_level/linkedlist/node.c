e2acaf657d040076a3feb70ec5e4396842559288
Praise Tompane <praisetompane@icloud.com>
2023-10-27 07:34:14 +0200

    refactor: tutorials
    separate toturials and application_projects. i want the application_projects to only house initiatives i began from conception to implementation(from blank slate(folder)

--- 0_theory_of_computation/3_design_and_analysis_of_algorithms/implementations/datastrutures/low_level/linkedlist/node.c
+++
@@ -1,9 +0,0 @@
-#include "../memory_cell/memory_allocator.h"
-#include "node.h"
-
-node * newnode(void *value, node *next) {
-    node *n = (node*)allocate_memory(sizeof(node));
-    n->value = value;
-    n->next = next;
-    return n;
-}
