24fefcaf8b34cc0f0b9f65717b6f4d1899193eb6
Praise Tompane <praisetompane@icloud.com>
2024-01-10 23:15:17 +0200

    feat(variables)
    mutable and constant values

---
+++ 5_software_and_its_engineering/1_software_notations_and_tools/1_general_programming_languages/javascript/tutorials/js_basics/browsers/1_variables.js
@@ -0,0 +1,12 @@
+console.log('pre ES6');
+var variable = 0;
+
+console.log('from ES6 and industry standard');
+let name;
+console.log('defaults to undefined, see console.log(name); result below');
+console.log(name);
+
+console.log('multiple variables');
+let firstName = 'Ben';
+let lastName = 'Smith';
+console.log(firstName, lastName);
