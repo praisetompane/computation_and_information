fec89ec6dc634faa0de7c5b265842c2f8c02bc2e
Praise Tompane <praisetompane@icloud.com>
2024-01-10 20:56:19 +0200

    feat(setup.sh)
    setup script for JavaScript environment and intial language references.

---
+++ 5_software_and_its_engineering/1_software_notations_and_tools/1_general_programming_languages/javascript/setup.sh
@@ -0,0 +1,23 @@
+echo "adding asdf nodejs plugin"
+asdf plugin add nodejs
+echo "done"
+
+echo "installing nodejs"
+asdf install nodejs latest
+echo "done"
+
+echo "set latest version to system wide version"
+asdf global nodejs latest
+echo "done"
+
+echo "adding asdf pnpm plugin"
+asdf plugin add pnpm
+echo "done"
+
+echo "installing pnpm"
+asdf install pnpm latest
+echo "done"
+
+echo "set latest version to system wide version"
+asdf global pnpm latest
+echo "done"
