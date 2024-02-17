0e4ffb8baf2df5b4a5e22fee244ab302f11830bb
Praise Tompane <praisetompane@icloud.com>
2024-02-17 22:10:21 +0200

    fix(git): set git pull to default to rebase
    --date=0.day.ago

--- 5_software_and_its_engineering/programming_environment_setup/git.sh
+++ 5_software_and_its_engineering/programming_environment_setup/git.sh
@@ -19,3 +19,7 @@ echo "Generaing SSH key"
 ssh-keygen -t ed25519 -C $email_address
 
 ssh-add ~/.ssh/id_ed25519
+
+echo "setting git pull to rebase default"
+git config --global pull.rebase true
+echo "done"
