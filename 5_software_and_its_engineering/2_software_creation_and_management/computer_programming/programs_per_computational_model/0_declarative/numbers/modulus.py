07d98b24f1b5d52bcbd2004998d6ff5e592beb76
Praise Tompane <praisetompane@icloud.com>
2025-02-09 09:10:16 +0200

    refactor(computer_programming): migrate again :) into separate repo to study computer programming using a Mathematical model. Also because it does not really fit well into the ACM categories

--- 5_software_and_its_engineering/2_software_creation_and_management/computer_programming/programs_per_computational_model/0_declarative/numbers/modulus.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-numbers = pd.DataFrame([10000, 2345, 620, 485, 135, 80, 55, 25, 5, 0])
-numbers.plot()
-
-plt.xlabel("iterations")
-plt.ylabel("remainders")
-plt.show()
