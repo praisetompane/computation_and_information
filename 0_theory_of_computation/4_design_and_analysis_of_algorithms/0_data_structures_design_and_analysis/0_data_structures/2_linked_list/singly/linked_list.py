dbf5206f27ef4a886407ee1a33f5fec94a762268
Praise Tompane <praisetompane@icloud.com>
2025-04-05 17:56:07 +0200

    fix: insert at top

--- 0_theory_of_computation/4_design_and_analysis_of_algorithms/0_data_structures_design_and_analysis/0_data_structures/2_linked_list/singly/linked_list.py
+++ 0_theory_of_computation/4_design_and_analysis_of_algorithms/0_data_structures_design_and_analysis/0_data_structures/2_linked_list/singly/linked_list.py
@@ -4,3 +4,11 @@ from singly.node import Node
 class LinkedList:
     def __init__(self):
         self.head = None
+
+    def insert_at_top(self, data):
+        node = Node(data)
+        if self.head:
+            node.next = self.head
+            self.head = node
+        else:
+            self.head = node
--- 0_theory_of_computation/4_design_and_analysis_of_algorithms/0_data_structures_design_and_analysis/0_data_structures/2_linked_list/singly/test_linked_list.py
+++ 0_theory_of_computation/4_design_and_analysis_of_algorithms/0_data_structures_design_and_analysis/0_data_structures/2_linked_list/singly/test_linked_list.py
@@ -1,6 +1,16 @@
 from singly.linked_list import LinkedList
+from singly.node import Node
 
 
 def test_construction():
     linked_list = LinkedList()
     assert linked_list.head == None
+
+
+def test_insert_at_top():
+    linked_list = LinkedList()
+    linked_list.insert_at_top(1)
+    linked_list.insert_at_top(2)
+
+    assert linked_list.head.data == 2
+    assert linked_list.head.next.data == 1
